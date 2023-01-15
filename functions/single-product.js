b09344940ff0a2cc181be1afe85562aa89520966
= <=>
2023-01-15 19:17:07 +0500

    single product bug fiexed

--- functions/single-product.js
+++ functions/single-product.js
@@ -9,17 +9,24 @@ const airtable = new Airtable({ apiKey: process.env.AIR_TABLE_API })
 exports.handler = async (event, context, cb) => {
     const { id } = event.queryStringParameters
     if (id) {
-        let product = await airtable.retrieve(id)
-        if (product.error) {
-            return {
-                statusCode: 404,
-                body: `no product with id : ${id}`
+        try {
+            let product = await airtable.retrieve(id)
+            if (product.error) {
+                return {
+                    statusCode: 404,
+                    body: `no product with id : ${id}`
+                }
             }
-        }
-        product = { id: product.id, ...product.fields }
-        return {
-            statusCode: 200,
-            body: JSON.stringify(product),
+            product = { id: product.id, ...product.fields }
+            return {
+                statusCode: 200,
+                body: JSON.stringify(product),
+            }
+        } catch (error) {
+            return {
+                statusCode: 500,
+                body: 'there was an error'
+            }
         }
     }
     return {
